f4c052d968c552db23cb3e1da40558822ee196b6
jezmu <jezmuenterprises@gmail.com>
2022-06-08 19:13:29 -0700

    commit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    copy = []
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            copy.append(replace)
-        else:
-            copy.append(my_list[i])
-    return 
+    new_list = [replace if item == search else item for item in my_list]
+    return new_list
