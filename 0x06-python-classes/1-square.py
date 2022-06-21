b65cef6528568cf351ba672a0340e196261635c5
jezmu <jezmuenterprises@gmail.com>
2022-06-21 09:40:55 -0700

    commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A module that declares a private instance variable: 'size'"""
+
+
+class Square:
+    """Square: A class that defines a square."""
+
+    def __init__(self, size):
+        """Constructor of the Square class
+        Args:
+            size (int): the size of the square
+        """
+        self.__size = 
